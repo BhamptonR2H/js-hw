d4c51241d2acb73fc0d1db6fbbc70c53b0369a5b
Brenden Hampton <brendenhampton@R2Hs-MacBook-Pro.local>
2022-01-03 18:21:35 -0500

    finished VE,Easy,Hard

---
+++ script.js
@@ -0,0 +1,42 @@
+// Very Easy
+x = 70
+y = 23
+console.log('the difference between 70 and 23 is', x - y )
+
+// Easy
+myvar = "Brenden"
+myvar2 = "Tyler"
+console.log(myvar + ' has 2 more letters than', myvar2)
+
+// Medium
+
+
+
+
+
+//Hard
+function sum(x,y){
+    return x + y;
+}
+
+console.log(3+6)
+
+function subtract(x,y){
+    return x - y;
+}
+
+console.log(20-9);
+
+function multiply(x,y){
+    return x * y;
+}
+
+console.log(10 * 10);
+
+function divide(x,y){
+    return x/y;
+}
+
+console.log(20/2);
+
+// Very hard
